6b99168b35e7228379ab4b152ab53b6022a79add
alien <eng@zaki.com>
2023-03-15 15:14:09 +0200

    Update database migrations (schema validators and index creation)

--- core/db.py
+++ core/db.py
@@ -1,16 +1,21 @@
-from pymongo import MongoClient, ASCENDING, DESCENDING
+import pymongo.errors
+from pymongo import MongoClient, ASCENDING, DESCENDING, errors
 from bson import ObjectId
 from dotenv import load_dotenv
 import os
+import pprint
+
 
 load_dotenv()
 config = os.environ
 client = MongoClient(config['MONGO_URI'])
 db = client['derzandb']
+vendors_collection = db['vendors']
+products_collection = db['products']
+product_urls_collection = db['product_urls']
 
 
-def migrate_collections():
-    # Vendors
+def migrate_vendor_collection():
     print('=> Migrating vendors..')
     try:
         vendors_collection = db.create_collection('vendors')
@@ -20,7 +25,7 @@ def migrate_collections():
         '$jsonSchema': {
             'bsonType': 'object',
             'title': 'Vendor Object Validation',
-            'required': ['name'],
+            'required': ['name', 'nickname', 'category', 'language'],
             'properties': {
                 'name': {
                     'bsonType': 'string',
@@ -28,17 +33,26 @@ def migrate_collections():
                 },
                 'nickname': {
                     'bsonType': 'string',
+                    'description': '"nickname" must be a string'
                 },
-                'website': {
+                'category': {
                     'bsonType': 'string',
+                    'description': '"category" must be a string'
+                },
+                'language': {
+                    'bsonType': 'string',
+                    'description': '"language" must be a string'
                 },
             }
         }
     }
+    # apply schema
     db.command('collMod', 'vendors', validator=vendor_validator)
+    # index creation
     vendors_collection.create_index([('name', ASCENDING)], unique=True)
 
-    # Products
+
+def migrate_product_collection():
     print('=> Migrating products..')
     try:
         products_collection = db.create_collection('products')
@@ -47,31 +61,58 @@ def migrate_collections():
     product_validator = {
         '$jsonSchema': {
             'bsonType': 'object',
-            'required': ['code', 'name', 'vendor_id', 'category', 'price'],
+            'required': ['vendor_id', 'code', 'name', 'price', 'currency', 'main_category', 'category'],
             'properties': {
+                'vendor_id': {
+                    'bsonType': 'objectId',
+                },
                 'code': {
                     'bsonType': 'string',
                 },
                 'name': {
                     'bsonType': 'string',
                 },
-                'vendor_id': {
-                    'bsonType': 'objectId',
-                },
-                'category': {
-                    'bsonType': 'string',
-                },
                 'price': {
                     'bsonType': 'double',
+                },
+                'list_price': {
+                    'bsonType': 'double',
+                },
+                'currency': {
+                    'bsonType': 'string',
+                },
+                'subcategory': {
+                    'bsonType': 'string',
+                },
+                'description': {
+                    'bsonType': 'string',
+                },
+                'variant_group': {
+                    'bsonType': 'string',
+                },
+                'variant_features': {
+                    'bsonType': 'array'
+                },
+                'url': {
+                    'bsonType': 'string',
+                },
+                'images': {
+                    'bsonType': 'array',
+                    'items': {
+                        'bsonType': 'string'
+                    }
                 },
             }
         }
     }
+    # apply schema
     db.command('collMod', 'products', validator=product_validator)
+    # index creation
     products_collection.create_index([('vendor_id', ASCENDING)])
     products_collection.create_index([('vendor_id', ASCENDING), ('code', ASCENDING)], unique=True)
 
-    # Images
+
+def migrate_image_collection():
     print('=> Migrating images..')
     try:
         images_collection = db.create_collection('images')
@@ -91,9 +132,11 @@ def migrate_collections():
             }
         }
     }
+    # apply schema
     db.command('collMod', 'images', validator=image_validator)
 
-    # Product URLs
+
+def migrate_product_urls_collection():
     print('=> Migrating product_urls..')
     try:
         product_urls_collection = db.create_collection('product_urls')
@@ -116,12 +159,28 @@ def migrate_collections():
             }
         }
     }
-    db.command('collMod', 'product_urls', validator=product_validator)
+    # apply schema
+    db.command('collMod', 'product_urls', validator=product_url_validator)
+    # index creation
     product_urls_collection.create_index([('url', ASCENDING)], unique=True)
     product_urls_collection.create_index([('vendor_id', ASCENDING), ('status', ASCENDING)])
+
+
+def migrate_all_collections():
+    # migrate vendor collection
+    migrate_vendor_collection()
+
+    # migrate product collection
+    migrate_product_collection()
+
+    # migrate image collection
+    # migrate_image_collection()
+
+    # migrate product URLs collection
+    migrate_product_urls_collection()
 
     print('=> Migrations completed!')
 
 
 if __name__ == '__main__':
-    migrate_collections()
+    migrate_all_collections()
